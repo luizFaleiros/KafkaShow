7b1b6dda354a60271d7b14190cac58fb232a0009
LuizFaleiros <luizhesafa21@gmail.com>
2025-11-01 15:28:55 -0300

    commit init

---
+++ src/main/java/br/com/kafkaShow/KafkaShowApplication.java
@@ -0,0 +1,13 @@
+package br.com.kafkaShow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaShowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaShowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/kafkaShow/KafkaShowApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.kafkaShow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaShowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
